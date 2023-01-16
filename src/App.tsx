a5906edb7983012c0ae4142ce001736703ff8a37
niiadoteibrown <samuelbrownai@gmail.com>
2023-01-16 13:19:59 +0000

    about section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Home from "./components/Home";
 import Navbar from "./components/NavBar";
+import About from "./components/About";
 
 function App() {
 	return (
 		<>
 			<Navbar />
 			<Home />
+			<About />
 		</>
 	);
 }
